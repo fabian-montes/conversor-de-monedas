f2647543f8adc0b1a7881e17fa6dcbd35e9a376a
Fabian Montes <fabianmontes11fab@gmail.com>
2024-05-03 21:59:22 -0600

    feat: crear main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+/*
+comandos de compilacion en CMD
+javac -d ./classes ./source/*.java
+java -cp ./classes Main
+*/
+public class Main {
+	public static void main(String [] args) {
+		System.out.println("Sea bienvenido/a al conversor de monedas c:");
+	}
+}
